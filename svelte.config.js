81b3da6795b217007fedb5425338b52dc3770d39
fardhanardhi <fa.dsg.id@gmail.com>
2025-03-30 13:06:51 +0700

    Add Shiki for enhanced code highlighting in markdown files

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,42 @@
-import { mdsvex } from 'mdsvex';
+import { mdsvex, escapeSvelte } from 'mdsvex';
+import { createHighlighter } from 'shiki';
 import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-  extensions: ['.md']
+  extensions: ['.md'],
+  highlight: {
+    highlighter: async (code, lang = 'text') => {
+      const highlighter = await createHighlighter({
+        themes: ['poimandres'],
+        langs: [
+          'javascript',
+          'typescript',
+          'powershell',
+          'shellscript',
+          'dart',
+          'yaml',
+          'json',
+          'jsonc',
+          'html'
+        ]
+      });
+      await highlighter.loadLanguage(
+        'javascript',
+        'typescript',
+        'powershell',
+        'shellscript',
+        'dart',
+        'yaml',
+        'json',
+        'jsonc',
+        'html'
+      );
+      const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }));
+      return `{@html \`${html}\` }`;
+    }
+  }
 };
 
 /** @type {import('@sveltejs/kit').Config} */
