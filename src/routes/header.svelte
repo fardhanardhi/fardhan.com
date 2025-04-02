83b278d26f762b02fef8c4a9da8af4ae47963674
fardhanardhi <fa.dsg.id@gmail.com>
2025-04-03 01:39:20 +0700

    feat: implement responsive drawer menu and enhance header layout

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -2,8 +2,11 @@
   import { page } from '$app/state';
   import { onMount } from 'svelte';
   import * as config from '$lib/config';
+  import { Menu } from 'lucide-svelte';
+  import clsx from 'clsx';
 
   let darkMode: boolean | null = $state(null);
+  let drawer = $state(false);
 
   onMount(() => {
     const savedDarkMode = localStorage.getItem('darkMode');
@@ -65,7 +68,7 @@
 <div class="{page.url.pathname === '/' ? 'header-home' : 'header'} anim">
   <div class="navbar-start">
     <div class="dropdown">
-      <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+      <div tabindex="0" role="button" class="btn btn-ghost hidden sm:max-lg:flex">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           class="h-5 w-5"
@@ -81,7 +84,7 @@
           />
         </svg>
       </div>
-      <ul class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+      <ul class="menu menu-xl dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
         {#each pages as link}
           <li><a href={link.url}>{link.name}</a></li>
         {/each}
@@ -117,7 +120,7 @@
         '/' && 'hidden sm:max-xl:flex lg:hidden'}">{config.title}</a
     >
 
-    <ul class="menu menu-horizontal hidden px-1 lg:flex">
+    <ul class="menu menu-lg menu-horizontal hidden px-1 lg:flex">
       {#each pages as link}
         <li><a href={link.url}>{link.name}</a></li>
       {/each}
@@ -136,7 +139,7 @@
     </ul>
   </div>
   <div class="navbar-end">
-    <div class="flex-1 pr-2 text-end xl:pr-4">
+    <div class="hidden flex-1 pr-2 text-end sm:inline xl:pr-4">
       <label class="swap swap-rotate duration-75 {darkMode === null && 'hidden'}">
         <input hidden type="checkbox" onclick={toggleDarkMode} bind:checked={darkMode} />
 
@@ -164,65 +167,73 @@
   </div>
 </div>
 
-<div class="pointer-events-none fixed top-0 right-0 h-svh content-end px-4 py-4">
-  <ul
-    class="menu menu-lg bg-base-100 border-base-content rounded-box pointer-events-auto border-2 shadow-xl"
-  >
-    <li>
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-          />
-        </svg>
-      </a>
-    </li>
-    <li>
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-      </a>
-    </li>
-    <li>
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-          />
-        </svg>
-      </a>
-    </li>
-  </ul>
+<div class="drawer drawer-end sm:hidden">
+  <input id="my-drawer-4" type="checkbox" class="drawer-toggle" bind:checked={drawer} />
+  <div class="drawer-content">
+    <!-- Page content here -->
+    <label
+      for="my-drawer-4"
+      class={clsx(
+        'drawer-button',
+        'btn',
+        'btn-xl',
+        'btn-accent',
+        'btn-circle',
+        'fixed',
+        'right-6',
+        'bottom-6',
+        'shadow-lg'
+      )}><Menu /></label
+    >
+  </div>
+  <div class="drawer-side">
+    <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
+    <div class="bg-base-200 text-base-content flex min-h-full w-80 items-end p-4">
+      <ul class="menu menu-xl flex w-full">
+        <!-- Sidebar content here -->
+        <div class=" mx-4 my-4 flex-1 pr-2">
+          <label class="swap swap-rotate duration-75 {darkMode === null && 'hidden'}">
+            <input hidden type="checkbox" onclick={toggleDarkMode} bind:checked={darkMode} />
+
+            <svg
+              class="swap-on text-warning h-10 w-10 fill-current delay-75 duration-300"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"
+              />
+            </svg>
+
+            <svg
+              class="swap-off text-warning-content h-10 w-10 fill-current delay-75 duration-300"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"
+              />
+            </svg>
+          </label>
+        </div>
+        <li>
+          <details>
+            <summary class="py-4">More</summary>
+            <ul class="p-2">
+              {#each more as link}
+                <li>
+                  <a class="py-4" href={link.url} onclick={() => (drawer = false)}>{link.name}</a>
+                </li>
+              {/each}
+            </ul>
+          </details>
+        </li>
+        {#each pages.reverse() as link}
+          <li><a class="py-4" href={link.url} onclick={() => (drawer = false)}>{link.name}</a></li>
+        {/each}
+      </ul>
+    </div>
+  </div>
 </div>
 
 <style lang="postcss">
@@ -243,8 +254,8 @@
   }
 
   .header {
-    @apply navbar hidden min-h-4;
-
+    @apply navbar min-h-4;
+    @apply hidden sm:flex;
     @apply text-base-content xl:text-base-content;
     @apply sticky top-0 xl:top-2;
     @apply bg-base-100/75 xl:bg-base-300/75 dark:bg-neutral/75;
