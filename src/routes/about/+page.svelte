103a2dfdcfaa3994efcaa13397f3458bdd7587f7
fardhanardhi <fa.dsg.id@gmail.com>
2025-04-12 00:34:46 +0700

    feat: add Coming Soon page and integrate it into About, Projects, Shorts, and Statistics routes

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import ComingSoon from '../coming-soon.svelte';
+  import * as config from '$lib/config';
+  import type { PageData } from './$types';
+
+  let { data }: { data: PageData } = $props();
+</script>
+
+<svelte:head>
+  <title>About - {config.pageTitle}</title>
+</svelte:head>
+
+<ComingSoon />
---
+++ src/routes/coming-soon.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { Construction } from 'lucide-svelte';
+</script>
+
+<div class=" flex min-h-100 flex-col items-center justify-center px-4">
+  <div class="text-error mb-6">
+    <Construction class="h-16 w-16" />
+  </div>
+
+  <h1 class="text-base-content mb-2 text-5xl font-bold">Coming Soon!</h1>
+  <p class="text-base-content/70 mb-8 text-xl">Stay Tune</p>
+
+  <a href="/" class="btn btn-outline"> Go back home </a>
+</div>
