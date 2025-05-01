056bff95d06c17003c3d90b68a383e8c549f2dd6
fardhanardhi <fa.dsg.id@gmail.com>
2025-05-01 23:21:56 +0700

    fix: simplify box shadow for profile background to enhance visual clarity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -187,8 +187,8 @@
     @apply transition-all;
     @apply duration-200;
     @apply md:h-40 md:w-40;
-    -webkit-box-shadow: 0 -30px 300px -25px var(--color-primary);
-    box-shadow: 0 -30px 300px -25px var(--color-primary);
+    -webkit-box-shadow: 0 -30px var(--color-primary);
+    box-shadow: 0 -30px var(--color-primary);
   }
   .icon-item {
     @apply bg-base-100;
